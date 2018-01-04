c1f2241592e4257e02d9129978c92c188e8edd6c
Florian Serraille <florian.serraille@gmail.com>
2018-01-04 01:08:06 -0200

    v0.20180103

--- cosmos/cosmos_extractor.sh
+++ cosmos/cosmos_extractor.sh
@@ -15,15 +15,42 @@ function _check_part_mount(){
 
 # O usuário escolha a(s) aplicação(ões)
 function _app(){
+	
 	_dialog.wipe
-    echo "Bom dia $SUDO_USER, escolha uma aplicacao:" > "${TMP_DIR}/dialog_message"
+    echo "Escolha uma ou mais aplicacoes:" > "${TMP_DIR}/dialog_title"
 	_db.ler_campo "1" > "${TMP_DIR}/dialog_tags"
-    _dialog.menu
-    #APP_NAME=$(echo "$DIALOG_ESCOLHA")
-    #_check_exit $(echo $EXIT_STATUS)
+    
+    _dialog.buildlist
+    _check_exit
+    EXTRACTOR_APP_ESCOLHA="$DIALOG_ESCOLHA"
+    
+    if  test -z "$EXTRACTOR_APP_ESCOLHA"; then
+    	_dialog.wipe
+        echo "Erro" > "${TMP_DIR}/dialog_title"
+        echo -e "\nEscolha pelo menos uma aplicacao\n" > "${TMP_DIR}/dialog_message"
+        _dialog.msgbox
+        _app
+    fi
+}
+
+function _check_exit(){
+    if [ "$EXIT_STATUS" -eq 1 ]; then                                                                                                                                                                                        
+        exit 1
+    fi
+}
+
+function _data(){
+  
+	_dialog.wipe
+	echo "Calendario" > "${TMP_DIR}/dialog_title"
+	echo -e "\nEscolhe a data dos logs desejado.\nEscolher 'Intervalo' para obter um range de data." > "${TMP_DIR}/dialog_message"
+	echo "Intervalo" > "${TMP_DIR}/dialog_extra_button"
+	_dialog.calendar
+
 }
 
 ##### MAIN #####
 
 _check_part_mount
-_app
+#_app
+_data
